9f3a357655f4896dd5d81c63bc72a2cd9dee2ae4
Kay Cee <se.kaycee@gmail.com>
2023-02-27 20:34:16 +0100

    edit app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,21 @@
 # will contain our backend logic
+''' Starter module '''
+from flask import Flask, abort, request
+from tempfile import NamedTemporaryFile
+import whisper
+import torch
 
+# Check if NVIDIA GPU is available
+torch.cuda.is_available()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load the Whisper model:
+model = whisper.load_model("base", device=DEVICE)
+
+app = Flask(__name__)
+
+
+# Create a route that will accept a post request with a file in it.
 @app.route("/")
 def hello():
     return "Whisper Hello World!"
