dc453b3ae2661c167d197da5dd11947eecdb1ef7
Jason Kvïst <94616615+Jasiel-Stark8@users.noreply.github.com>
2023-02-27 20:34:14 +0000

    Update gpt3.py

--- server/gpt3.py
+++ server/gpt3.py
@@ -6,8 +6,8 @@ def gpt3complete(speech):
     # Completion function call engine: text-davinci-002
 
     Platformresponse = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt="Write a short summary of this text: {}".format(speech),
+        engine="text-davinci-003",
+        prompt="Write a short summary of this text: {}".format(speech), #edit prompt message 
         temperature=0.7,
         max_tokens=1500,
         top_p=1,
